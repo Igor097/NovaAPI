7d3db18b3b4ff73e78418bb0f226caae85e76161
Igor Fernandes <fernandesigor09@gmail.com>
2021-09-29 16:56:43 -0300

    Changing the method name from getAll to getAllUsers

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-	getAll(): string {
+	getAllUsers(): string {
 		return 'GetAll';
 	}
 	getUserById(): string {
