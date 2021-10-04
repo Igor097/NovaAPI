98a903d9352a3368c674df8ac949a35d6a2e1196
Igor Fernandes <fernandesigor09@gmail.com>
2021-10-04 17:51:43 -0300

    Implementing the methods in the user service file

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
-	getAllUsers(): string {
-		return 'GetAll';
+	constructor(private prisma: PrismaService) {}
+	async getAllUsers(params: {
+		skip?: number;
+		take?: number;
+		cursor?: Prisma.UserWhereUniqueInput;
+		where?: Prisma.UserWhereInput;
+		orderBy?: Prisma.UserOrderByWithRelationInput;
+	}): Promise<User[]> {
+		const { skip, take, cursor, where, orderBy } = params;
+		return this.prisma.user.findMany({
+			skip,
+			take,
+			cursor,
+			where,
+			orderBy,
+		});
 	}
-	getUserById(): string {
-		return 'GetUserById';
+	async getUserById(user: Prisma.UserWhereUniqueInput): Promise<User | null> {
+		return this.prisma.user.findUnique({
+			where: user,
+		});
 	}
-	createUser(): string {
-		return 'CreateUser';
+	async createUser(data: Prisma.UserCreateInput): Promise<User> {
+		return this.prisma.user.create({
+			data,
+		});
 	}
-	updateUser(): string {
-		return 'UpdateUser';
+	updateUser(params: {
+		where: Prisma.UserWhereUniqueInput;
+		data: Prisma.UserUpdateInput;
+	}): Promise<User> {
+		const { where, data } = params;
+		return this.prisma.user.update({
+			data,
+			where,
+		});
 	}
-	deleteUser(): string {
-		return 'DeleteUser';
+	deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+		return this.prisma.user.delete({
+			where,
+		});
 	}
 }
