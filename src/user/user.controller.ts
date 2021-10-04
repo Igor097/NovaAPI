eeb5dc692ee5116d068d6fdaeabbd85da729ccad
Igor Fernandes <fernandesigor09@gmail.com>
2021-10-04 17:53:24 -0300

    Implementing the methods in the user controller file

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,34 +7,67 @@ import {
 	Post,
 	Put,
 } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-	constructor(private userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
 
 	@Get()
-	getAllUsers(): string {
-		return this.userService.getAllUsers();
+	async getAllUsers(): Promise<User[]> {
+		return this.userService.getAllUsers({});
 	}
 
 	@Get(':id')
-	getUserById(@Param() params): string {
-		return this.userService.getUserById();
+	async getUserById(@Param() params): Promise<User> {
+		return this.userService.getUserById({
+			userId: Number(params.id),
+		});
 	}
 
 	@Post()
-	createUser(@Body() user): string {
-		return this.userService.createUser();
+	async createUser(
+		@Body()
+		user: {
+			firstName: string;
+			lastName: string;
+			email: string;
+			description: string;
+			phoneNumber: string;
+			birthdayDate: Date;
+		},
+	): Promise<User> {
+		const {
+			firstName,
+			lastName,
+			email,
+			description,
+			phoneNumber,
+			birthdayDate,
+		} = user;
+		return this.userService.createUser({
+			firstName,
+			lastName,
+			email,
+			description,
+			phoneNumber,
+			birthdayDate,
+		});
 	}
 
-	@Put()
-	updateUser(@Body() user): string {
-		return this.userService.updateUser();
+	@Put('id')
+	async updateUser(@Body() user): Promise<User> {
+		return this.userService.updateUser({
+			where: { userId: Number(user.id) },
+			data: { ...user },
+		});
 	}
 
 	@Delete(':id')
-	deleteUser(@Param() params): string {
-		return this.userService.deleteUser();
+	async deleteUser(@Param() params): Promise<User> {
+		return this.userService.deleteUser({
+			userId: Number(params.id),
+		});
 	}
 }
