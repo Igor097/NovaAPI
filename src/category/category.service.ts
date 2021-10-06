e77fda30856b58eaa723143246ad335d59456969
Igor Fernandes <fernandesigor09@gmail.com>
2021-10-05 23:04:20 -0300

    Implementing the methods

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,20 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { Category, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class CategoryService {
-	getAllCategories(): string {
-		return 'GetAllCategories';
+	constructor(private prisma: PrismaService) {}
+	async getAllCategories(params: {
+		skip?: number;
+		take?: number;
+		cursor?: Prisma.CategoryWhereUniqueInput;
+		where?: Prisma.CategoryWhereInput;
+		orderBy?: Prisma.CategoryOrderByWithRelationInput;
+	}): Promise<Category[]> {
+		const { skip, take, cursor, where, orderBy } = params;
+		return this.prisma.category.findMany({
+			skip,
+			take,
+			cursor,
+			where,
+			orderBy,
+		});
 	}
-	getCategoryById(): string {
-		return 'GetCategoryById';
+	async getCategoryById(
+		category: Prisma.CategoryWhereUniqueInput,
+	): Promise<Category> {
+		return this.prisma.category.findUnique({
+			where: category,
+		});
 	}
-	createCategory(): string {
-		return 'CreateCategory';
+	async createCategory(data: Prisma.CategoryCreateInput): Promise<Category> {
+		return this.prisma.category.create({
+			data,
+		});
 	}
-	updateCategory(): string {
-		return 'UpdateCategory';
+	async updateCategory(params: {
+		where: Prisma.CategoryWhereUniqueInput;
+		data: Prisma.CategoryUpdateInput;
+	}): Promise<Category> {
+		const { where, data } = params;
+		return this.prisma.category.update({
+			data,
+			where,
+		});
 	}
-	deleteCategory(): string {
-		return 'DeleteCategory';
+	async deleteCategory(
+		where: Prisma.CategoryWhereUniqueInput,
+	): Promise<Category> {
+		return this.prisma.category.delete({
+			where,
+		});
 	}
 }
